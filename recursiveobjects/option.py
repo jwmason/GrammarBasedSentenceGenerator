df4ae6cfb005fd4f46e754d0bb05923450a0f5c2
Mason Wong <masonjw1@uci.edu>
2023-06-05 17:22:39 -0700

    option.py
    Created the generate_sentence_fragment() function in Option class. The function goes through every symbol and calls on the symbol generate_sentence_fragment() function, ultimately achieving recursion. Also made some more unittests for this module, achieving full code coverage for this module.

--- recursiveobjects/option.py
+++ recursiveobjects/option.py
@@ -1,9 +1,14 @@
 """This module is in charge of handling options"""
 
 class Option:
+    """This class creates an Option object"""
     def __init__(self, probability, symbols):
+        """This initializes the attributes of the Option object"""
         self.probability = probability
         self.symbols = symbols
 
     def generate_sentence_fragment(self):
-        pass
+        """This function generates sentence fragment based on if the symbol
+        is a Terminal or Variable Symbol"""
+        for symbol in self.symbols:
+            yield from symbol.generate_sentence_fragment()
--- tests/test_recursiveobjects/test_option.py
+++ tests/test_recursiveobjects/test_option.py
@@ -2,6 +2,7 @@
 
 import unittest
 from recursiveobjects.option import Option
+from recursiveobjects.terminalsymbol import TerminalSymbol
 
 
 class TestOption(unittest.TestCase):
@@ -11,3 +12,17 @@ class TestOption(unittest.TestCase):
         test_option = Option(1, 'TerminalSymbolPlaceHolder')
         self.assertEqual(test_option.probability, 1)
         self.assertEqual(test_option.symbols, 'TerminalSymbolPlaceHolder')
+
+    def test_generate_sentence_fragment(self):
+        """This function tests generate_sentence_fragment()"""
+        terminal = TerminalSymbol('test')
+        test_option = Option(1, [terminal])
+        generator = test_option.generate_sentence_fragment()
+        sentence_fragment = next(generator)
+        self.assertEqual(sentence_fragment, 'test')
+
+    def test_generate_sentence_fragment_no_symbols(self):
+        """This function tests generate_sentence_fragment() with no symbols"""
+        test_option = Option(1, [])
+        generator = test_option.generate_sentence_fragment()
+        self.assertRaises(StopIteration, next, generator)
