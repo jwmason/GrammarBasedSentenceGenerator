bfe3962141f9b001ef5cb6d2175c0cf97ab8b9dd
Mason Wong <masonjw1@uci.edu>
2023-06-05 18:12:56 -0700

    grammar.py
    Made unittests for add_rule() and get_rule() functions that I used in other recursive objects to get certain attributes for their own searching.

--- tests/test_recursiveobjects/test_grammar.py
+++ tests/test_recursiveobjects/test_grammar.py
@@ -2,6 +2,9 @@
 
 import unittest
 from recursiveobjects.grammar import Grammar
+from recursiveobjects.rule import Rule
+from recursiveobjects.option import Option
+from recursiveobjects.terminalsymbol import TerminalSymbol
 
 
 class TestGrammar(unittest.TestCase):
@@ -11,3 +14,22 @@ class TestGrammar(unittest.TestCase):
         test_grammar = Grammar()
         test_grammar.rules['testkey'] = 'testvalue'
         self.assertEqual(test_grammar.rules, {'testkey': 'testvalue'})
+
+    def test_add_rule(self):
+        """This function tests add_rule()"""
+        test_grammar = Grammar()
+        test_symbol = TerminalSymbol('testsymbol')
+        test_option = Option(1, [test_symbol])
+        rule = Rule('A', test_option)
+        test_grammar.add_rule(rule)
+        self.assertEqual(test_grammar.rules['A'], rule)
+
+    def test_get_rule(self):
+        """This function tests get_rule()"""
+        test_grammar = Grammar()
+        test_symbol = TerminalSymbol('testsymbol')
+        test_option = Option(1, [test_symbol])
+        rule = Rule('A', test_option)
+        test_grammar.add_rule(rule)
+        retrieved_rule = test_grammar.get_rule('A')
+        self.assertEqual(retrieved_rule, rule)
